655fcb8e46ca43a4cb480b144d1daa0072c1b64f
knbknb <knb@tnp-online.de>
2013-02-15 15:51:05 +0100

    addded 1 utils file with 3 hellper functions

---
+++ scripts/utils_text_mining.R
@@ -0,0 +1,62 @@
+# utils_text_mining.R:
+# Utilities for text mining AGU Fall meeting abstracts.
+# 
+
+
+########################################
+## Put everything into an environment, to not pollute global namespace
+
+text_mining_util = new.env()
+
+
+
+
+########################################
+## 
+text_mining_util$earthsci_stopwords = function(){
+	#return a vector
+	c( "data", "earth", "science", "using" ) # "(..,"
+}
+
+text_mining_util$rmPunc =  function(x){
+	# lookbehinds :
+	# need to be careful to specify fixed-width conditions 
+	# so that it can be used in lookbehind
+	x <- gsub('(.*?)(?<=^[[:space:][:punct:]’“”\\|:±</>]{9})([[:alnum:]])',"\\2", x, perl=TRUE) ;
+	x <- gsub('(.*?)(?<=^[[:space:][:punct:]’“”:±</>]{8})([[:alnum:]])',"\\2", x, perl=TRUE) ;
+	x <- gsub('(.*?)(?<=^[[:space:][:punct:]’“”\\|:±</>]{7})([[:alnum:]])',"\\2", x, perl=TRUE) ;
+	x <- gsub('(.*?)(?<=^[[:space:][:punct:]’“”:±</>]{6})([[:alnum:]])',"\\2", x, perl=TRUE) ;
+	x <- gsub('(.*?)(?<=^[[:space:][:punct:]’“”\\|:±</>]{5})([[:alnum:]])',"\\2", x, perl=TRUE) ;
+	x <- gsub('(.*?)(?<=^[[:space:][:punct:]’“”:±</>]{4})([[:alnum:]])',"\\2", x, perl=TRUE) ;
+	x <- gsub('(.*?)(?<=^[[:space:][:punct:]’“”:±</>]{3})([[:alnum:]])',"\\2", x, perl=TRUE) ;
+	x <- gsub('(.*?)(?<=^[[:space:][:punct:]’“”:±</>]{2})([[:alnum:]])',"\\2", x, perl=TRUE) ;
+	x <- gsub('(.*?)(?<=^[[:space:][:punct:]’“”:±</>])([[:alnum:]])',"\\2", x, perl=TRUE) ; 
+	# lookbehind there must be a word char to the left and punct/whitespace stuff to the end.
+	# append 1 extra blank 
+	x <- gsub('(.*?)(?<=[[:alnum:]])([[:space:][:punct:]’“”:±</>\\\\]+?)$',"\\1", x, perl=TRUE)
+	
+	# remove all strings that consist *only* of punct chars 
+	x <- gsub('^[[:space:][:punct:]’“”:±</>\\\\]+$',"", x, perl=TRUE) ;
+	x
+	
+}
+
+text_mining_util$cleanup = function(doc, sep= " "){
+	doc = gsub("body:", "", doc, perl=TRUE);
+	y = strsplit(doc, sep); 
+	
+	y = lapply(y, rmPunc); 
+	y[grep("\\S+", y, invert=FALSE, perl=TRUE)]; 
+	
+	y = sapply(y, paste, sep=" ")
+	paste(y, collapse = sep)
+	#y
+}
+
+
+########################################
+## Has to be last in file
+
+while("text_mining_util" %in% search())
+	detach("text_mining_util")
+attach(text_mining_util)
