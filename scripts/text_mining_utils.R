b1e4679b1b070079f699493e3442c02ec755d579
knbknb <knb@tnp-online.de>
2013-02-20 18:32:37 +0100

    Add wordcloud code

--- scripts/text_mining_utils.R
+++ scripts/text_mining_utils.R
@@ -47,15 +47,34 @@ text_mining_util$rmPunc =  function(x){
 	
 }
 
+#create a wordcloud. input a data frame with columns "word" and "freq"
+# a filename 
+text_mining_util$create_wordcloud_png = function(df, fn="wordcloud.png", minfreq=2, maxfreq=Inf){
+	png(filename, width=1280,height=800)
+	#wordcloud(df$word,df$freq,c(8,.3),2,100,TRUE,.15, pal,vfont=c("sans serif","plain"))
+	wordcloud(df$word,df$freq,scale=c(8,.3),min.freq=2,max.words=Inf,random.order=TRUE,rot.per=.15,colors=pal,vfont=c("sans serif","plain"))
+	dev.off()
+}
+
+# create many png files
+text_mining_util$wordclouds_pngs = function(df, fn="wordcloud", minfreq=2, maxfreq=Inf, seq=c(10,20,30,40,50, 100, 200)){
+	
+	lapply(seq, function(x){
+				text_mining_util$create_wordclouds_png(df, fn=paste0("wordcloud-", x, ".png"), minfreq=minfreq, maxfreq=maxfreq)
+			})
+	
+}
+
+
 text_mining_util$cleanup = function(doc, sep= " "){
 	doc = gsub("body:", "", doc, perl=TRUE);
 	y = strsplit(doc, sep); 
 	
 	y = lapply(y, text_mining_util$rmPunc); 
-	#y[grep("\\S+", y, invert=FALSE, perl=TRUE)]; 
+	y[grep("\\S+", y, invert=FALSE, perl=TRUE)]; 
 	
-	#y = sapply(y, paste, sep=" ")
-	#paste(y, collapse = sep)
+	y = sapply(y, paste, sep=" ")
+	paste(y, collapse = sep)
 	y
 }
 
