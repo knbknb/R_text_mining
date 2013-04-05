76f8d5a87fe28d8863e0ae847645e93ff5cd9b7b
knbknb <knb@tnp-online.de>
2013-04-05 16:31:07 +0200

    Modify hardcoded outfile name as needed

--- scripts/montage_pngs.sh
+++ scripts/montage_pngs.sh
@@ -7,8 +7,10 @@
 # and firefox must be in your PATH
 #
 # fn="AGU-hydrogeology. Common 10,20,30,40,50 terms" ; 
-# montage corpus-agu-hydrology.RData-1*.png corpus-agu-hydrology.RData-2*.png -tile x2 -geometry 1024x1024\>+10+10 -gravity center -crop 2600x2600+0+400  "$fn" ; convert -rotate 90 "$fn" "$fn-90" ; convert  -crop 1x5@  +repage  +adjoin "$fn-90" -rotate -90 -gravity South -annotate +0+10 "$fn" -rotate 90  "$fn.png"
-# ls -1 A*.png | xargs -i firefox &
+fn="AGU-volcanology Common 10,20,30,40,50 terms" ; 
+ montage c*-1*.png c*-2*.png -tile x2 -geometry 1024x1024\>+10+10 -gravity center -crop 2600x2600+0+400  "$fn" ; convert -rotate 90 "$fn" "$fn-90" ; convert  -crop 1x5@  +repage  +adjoin "$fn-90" -rotate -90 -gravity South -annotate +0+10 "$fn" -rotate 90  "$fn.png"
+rm "$fn" "$fn-90"
+ls -1 A*.png | xargs -i firefox "{}" &
 #
 # send to line printer 
 # print them xargs -i lpr "{}" -P ICDP_color_A69_hp -T "{}"
