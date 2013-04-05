ed98dd9780e3c1683d8f6fb1c41cd45d7846be37
knbknb <knb@tnp-online.de>
2013-04-05 10:05:54 +0200

    Add two proprietary helper scripts

---
+++ scripts/montage_pngs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#simple one-liner script to copy pngs together in single 2-row png,
+# and split them apart again into tiles of 1x2 pngs per tile, print hem out  
+#
+# imagemagick must be installed, 
+# and firefox must be in your PATH
+#
+# fn="AGU-hydrogeology. Common 10,20,30,40,50 terms" ; 
+# montage corpus-agu-hydrology.RData-1*.png corpus-agu-hydrology.RData-2*.png -tile x2 -geometry 1024x1024\>+10+10 -gravity center -crop 2600x2600+0+400  "$fn" ; convert -rotate 90 "$fn" "$fn-90" ; convert  -crop 1x5@  +repage  +adjoin "$fn-90" -rotate -90 -gravity South -annotate +0+10 "$fn" -rotate 90  "$fn.png"
+# ls -1 A*.png | xargs -i firefox &
+#
+# send to line printer 
+# print them xargs -i lpr "{}" -P ICDP_color_A69_hp -T "{}"
---
+++ scripts/show_stackoverflow_data.R
@@ -0,0 +1,4 @@
+# read in popular langs from stackoverflow.com, early 2013
+#setwd("~/code/svn/eclipse38_dynlang/R_one-offs/R_text_mining/data_public")
+#df = read.csv("stackoverflow_proglang_tag_count.txt", sep=" ")
+#head(df$langName, 10)
